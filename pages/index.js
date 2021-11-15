25dff5bc102aaf968f37efc88348fba86487ea01
Sanket Bagad <alencolins@gmail.com>
2021-11-15 17:14:17 +0530

    Some fews fugs fixed :(

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
-        <title>SB Blog</title>
+        <title>Sanket Bagad's BlogPost</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
